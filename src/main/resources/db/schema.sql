f505aaf5a9d2bba172a2fe14bc8c765c24567858
smallsweetsweet1 <1007512569@qq.com>
2019-10-12 17:24:56 +0800

    db change

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -29,3 +29,16 @@ CREATE TABLE `XGX` (
   `avatar_url` varchar(200) DEFAULT '' comment '头像',
   PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `FZQ` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL comment '用户名',
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `register_date` timestamp DEFAULT CURRENT_TIMESTAMP,
+  `enabled` tinyint(1) DEFAULT 1,
+  `role` varchar(50) DEFAULT NULL comment '角色',
+  `avatar_url` varchar(200) DEFAULT '' comment '头像',
+  PRIMARY KEY (`id`)
+);
