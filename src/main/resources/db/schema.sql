ca5d641a3206fe56a267578e6f6f520bd9a13896
xxx0527 <3466392059@qq.com>
2019-10-13 15:56:02 +0800

    用户管理

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -20,12 +20,13 @@ ALTER TABLE `user` ADD UNIQUE (`phone`);
 CREATE TABLE `XGX` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) DEFAULT NULL comment '用户名',
-  `password` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL comment '用户密码',
+  `sex` varchar(255) DEFAULT NULL comment '性别',
   `email` varchar(50) DEFAULT NULL,
   `phone` varchar(50) DEFAULT NULL,
   `register_date` timestamp DEFAULT CURRENT_TIMESTAMP,
   `enabled` tinyint(1) DEFAULT 1,
-  `role` varchar(50) DEFAULT NULL comment '角色',
+  `age` varchar(50) DEFAULT NULL comment '巴龄',
   `avatar_url` varchar(200) DEFAULT '' comment '头像',
   PRIMARY KEY (`id`)
 );
