9729e049d7505b7e91d75f9d469b76cbfa2ac48d
heying1 <you@example.com>
2019-10-17 17:34:41 +0800

    hy

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -17,7 +17,22 @@ ALTER TABLE `user` ADD UNIQUE (`username`);
 ALTER TABLE `user` ADD UNIQUE (`email`);
 ALTER TABLE `user` ADD UNIQUE (`phone`);
 
+<<<<<<< HEAD
 CREATE table 'intro'(
   'id'bigint(20) NOT NULL AUTO_INCREMENT,
   'introduction'varchar(200)DEFAULT NULL,
 );
+=======
+CREATE TABLE `XGX` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL comment '用户名',
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `register_date` timestamp DEFAULT CURRENT_TIMESTAMP,
+  `enabled` tinyint(1) DEFAULT 1,
+  `role` varchar(50) DEFAULT NULL comment '角色',
+  `avatar_url` varchar(200) DEFAULT '' comment '头像',
+  PRIMARY KEY (`id`)
+);
+>>>>>>> c52e4d20dba61292b83233677caf35e4130891c2
