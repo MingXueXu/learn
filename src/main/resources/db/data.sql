c37ebfaaced61657d3364888a2d7c4429f668104
Your Name <1007512569@qq.com>
2019-10-21 16:47:05 +0800

    change of col name

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO `user` (userid, username, password, register_date,avatar_url) VALUES ('1', 'user1', '1234', '2017-10-14 12:00:00','url');
 INSERT INTO `user` (userid, username, password, register_date,avatar_url) VALUES ('2', 'user2', '1234', '2017-10-14 12:00:00','url');
 
-INSERT INTO `video` (video_id, video_name, video_kind, video_like, video_gift, video_collect, video_click, upload_date) VALUES ('1', 'video1', '1234', '1234', '1234', '1234', '1234', '2017-10-14 12:00:00');
-INSERT INTO `video` (video_id, video_name, video_kind, video_like, video_gift, video_collect, video_click, upload_date) VALUES ('2', 'video2', '1234', '1234', '1234', '1234', '1234', '2017-10-14 12:00:00');
+INSERT INTO `video` (video_id, video_name, video_kind, video_liked, video_gift, video_collected, video_clicked, upload_date) VALUES ('1', 'video1', '1234', '1234', '1234', '1234', '1234', '2017-10-14 12:00:00');
+INSERT INTO `video` (video_id, video_name, video_kind, video_liked, video_gift, video_collected, video_clicked, upload_date) VALUES ('2', 'video2', '1234', '1234', '1234', '1234', '1234', '2017-10-14 12:00:00');
 
 COMMIT;
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -16,11 +16,12 @@ CREATE TABLE `video` (
   `video_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `video_name` varchar(50) DEFAULT NULL comment '视频名称',
   `video_kind` varchar(50) DEFAULT NULL comment '视频类型',
-  `video_like` varchar(2550) DEFAULT NULL comment '视频点赞数',
+  `video_liked` varchar(2550) DEFAULT NULL comment '视频点赞数',
   `video_gift` varchar(50) DEFAULT NULL comment '视频送花数',
-  `video_collect` varchar(50) DEFAULT NULL comment '视频收藏数',
-  `video_click` varchar(50) DEFAULT NULL comment '视频播放数',
+  `video_collected` varchar(50) DEFAULT NULL comment '视频收藏数',
+  `video_clicked` varchar(50) DEFAULT NULL comment '视频播放数',
   `upload_date` timestamp DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`video_id`)
 );
+ALTER TABLE `video` ADD UNIQUE (`video_id`);
 
